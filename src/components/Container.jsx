444ac77b16a5274f12b4c046095b72647d6a641b
Heitor Kretzer <heitor_kretzer@estudante.sesisenai.org.br>
2024-05-07 11:31:19 -0300

    flexyemedia

---
+++ src/components/Container.jsx
@@ -0,0 +1,16 @@
+export default function Container(){
+    return(
+        <div className="flexbox">
+            <div className="figueira">
+                <p> FIGUEIRENSE</p>
+            </div>
+            <div className="bvai">
+                <p> AVAI </p>
+            </div>
+            <div className="ciuma">
+                <p> CRICIUMA </p>
+            </div>
+        </div>
+        
+    )
+}
---
+++ src/components/Maior.jsx
@@ -0,0 +1,8 @@
+export default function Maior(){
+    return(
+        <div className="foto">
+            <img src="https://sportbuzz.uol.com.br/media/_versions/emblemas-de-times-de-santa-catarina_widelg.png" alt="" />
+        </div>
+        
+    )
+}
